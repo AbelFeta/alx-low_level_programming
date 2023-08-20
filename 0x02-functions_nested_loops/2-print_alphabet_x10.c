cefe9fe7857c8d7cbb035611d3ddde13dcedf670
AbelFeta <meetabelhere@gmail.com>
2023-08-20 06:10:27 -0700

    Create: create a code to print lowercase alphabets ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - prints all alphabets in lower case ten times
+*
+*Return: void
+*/
+
+void print_alphabet_x10(void)
+{
+char l;
+int i;
+for(i=0; i<10; i++)
+{
+l='a';
+while(l<='z')
+{
+_putchar(l);
+l++;
+}
+_putchar('\n');
+}
+}
